f9a70cccfbef24754052f17915b2f63a194cfceb
Mirishya <campanocrespomiriam@gmail.com>
2021-07-21 13:55:18 +0200

    heroku

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-var cool = require("cool-ascii-faces");
+//var cool = require("cool-ascii-faces");
 var express = require("express");
-
+var path = require("path");
 var app = express();
 
 var port = 10000;
 
-app.get("/cool", (request,response) => {
-    response.send(cool());
-    console.log("New request has arrived");
+app.use("/", express.static(path.join(__dirname + "/public")));
+
+app.get("/", (req,res) => {
+    //response.send(cool());
+    res.send("Hello from this tiny server");
+   // console.log("New request has arrived");
+});
+
+app.post("/", (req,res) => {
+  
+    res.send("Hello from this tiny server");
+  
 });
 
 app.listen(port, () => {
-    console.log("Server ready listening on port " + port);
+    console.log(`Server ready listening on ${port}`);
 });
-console.log(cool());
+//console.log(cool());
