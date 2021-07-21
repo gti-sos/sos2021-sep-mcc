3f4e66630c1e49cf5cc39fbaf5ffe16aed104376
Mirishya <campanocrespomiriam@gmail.com>
2021-07-21 14:52:04 +0200

    path

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-var port = 10000;
+var port = (process.env.PORT || 10000);
 
-app.use("/", express.static(path.join(__dirname + "/public")));
+app.use("/", express.static(path.join(__dirname,"public")));
 
 app.get("/", (req,res) => {
     //response.send(cool());
